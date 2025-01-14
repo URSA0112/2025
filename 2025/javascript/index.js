a0a791b58e07028ca0aebc936c20fb0d25fec1ca
URSA0112 <Nimone99@gmail.com>
2025-01-14 12:42:05 +0800

    json-learned, file organized, npm installed chalk

--- 2025/javascript/index.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs'
-fs.writeFile('./exapmle.txt', 'hello world', (err)=>{
-    if(err){console.log('error writing file:', err);
-        return;
-    }
-    console.log('successs');
-});
