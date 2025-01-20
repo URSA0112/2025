8f47f816c04fdccb7e2ef614f3bca264b5bf2c19
URSA0112 <Nimone99@gmail.com>
2025-01-20 16:32:43 +0800

    background color using  onclick changed

--- classworks/ball.js
+++ classworks/ball.js
@@ -1,34 +1,18 @@
-//const buttonElRef = document.createElement('button')
-//document.body.appendChild(buttonElRef)
-//buttonElRef.style.width = 100 +'px'
-//buttonElRef.style.height = 100 +'px'
-//buttonElRef.style.backgroundColor = "grey"
-//
-//const bodyElRef = document.getElementsByTagName('body')[0]
-//let bodycolor = bodyElRef.style.backgroundColor = "green"
-//
-//buttonElRef.onclick = function hello(color){
-// bodyElRef.style.backgroundColor = color;
-//}
-//
-//hello('purple')
+const buttonElRef = document.createElement('button')
+document.body.appendChild(buttonElRef)
+buttonElRef.style.width = 100 + 'px'
+buttonElRef.style.height = 100 + 'px'
+buttonElRef.style.backgroundColor = "grey"
 
-function hello(color) {
-    bodyElRef.style.backgroundColor = color;
-  }
-  
-  const buttonElRef = document.createElement('button');
-  document.body.appendChild(buttonElRef);
-  buttonElRef.style.width = 100 + 'px';
-  buttonElRef.style.height = 100 + 'px';
-  buttonElRef.style.backgroundColor = "grey";
-  
-  const bodyElRef = document.getElementsByTagName('body')[0];
-  bodyElRef.style.backgroundColor = "green";
-  
-  buttonElRef.onclick = function () {
-    hello('purple'); // Use the reusable function
-  };
-  
-  // You can also call hello directly:
-  hello('blue'); // Changes body color to blue
+const bodyElRef = document.getElementsByTagName('body')[0]
+let bodycolor = bodyElRef.style.backgroundColor = "green"
+
+const arr = ['white', 'black', 'lightgreen', 'blue'];
+for (let i = 0; i < arr.length; i++) {
+  (function(index){
+    buttonElRef.onclick = function () {
+      bodyElRef.style.backgroundColor = arr[index];
+      index = (index + 1) % arr.length; 
+    }
+  }) (i);
+}
