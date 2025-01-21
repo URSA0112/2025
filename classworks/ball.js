a5c750a8762ed7338e0f5d1fd4e0d97f2d1c840a
URSA0112 <Nimone99@gmail.com>
2025-01-21 12:01:24 +0800

    tip calculator

--- classworks/ball.js
+++ classworks/ball.js
@@ -1,18 +1,26 @@
-const buttonElRef = document.createElement('button')
-document.body.appendChild(buttonElRef)
-buttonElRef.style.width = 100 + 'px'
-buttonElRef.style.height = 100 + 'px'
-buttonElRef.style.backgroundColor = "grey"
+//document.body.style.backgroundColor = 'black'
 
-const bodyElRef = document.getElementsByTagName('body')[0]
-let bodycolor = bodyElRef.style.backgroundColor = "green"
+//percentage = (parts/whole)*10
 
-const arr = ['white', 'black', 'lightgreen', 'blue'];
-for (let i = 0; i < arr.length; i++) {
-  (function(index){
-    buttonElRef.onclick = function () {
-      bodyElRef.style.backgroundColor = arr[index];
-      index = (index + 1) % arr.length; 
-    }
-  }) (i);
-}
+ let percent = document.getElementsByClassName("btn");
+ 
+//function percentage(inputnum) {
+//  let value = (percent * inputnum) / 100;
+//  let addednum = inputnum + value;
+//  return addednum;
+//}
+//console.log(percentage(100, 5));
+
+//percent = buttons
+//input num = write on display
+//
+const BtncontainerElRef = document.getElementById('buttoncontainer')
+
+BtncontainerElRef.addEventListener("onclick",function(){
+    console.log(BtncontainerElRef.innerText)
+});
+//const inputValue = document.getElementById("inputnum");
+//console.log(inputValue.value);
+//inputValue.addEventListener("input", function(){
+//    console.log(inputValue.value)
+//})
